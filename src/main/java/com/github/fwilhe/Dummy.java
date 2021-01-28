c0a651dcf808ba0efc19af97eb038e136c346ed3
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-01-28 20:17:01 +0100

    bootstrap

--- src/main/java/com/github/fwilhe/Dummy.java
+++ src/main/java/com/github/fwilhe/Dummy.java
@@ -1,8 +1,21 @@
 package com.github.fwilhe;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.wasmer.*;
+
 public class Dummy {
-    public static void main(String[] args){
-        System.out.println("Hello");
+    public static void main(String[] args) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get("simple.wasm"));
+        Instance instance = new Instance(bytes);
+
+        Integer result = (Integer) instance.exports.getFunction("sum").apply(1, 2)[0];
+
+        System.out.println(result.toString());
+
+        assert result == 3;
+
+        instance.close();
     }
-    
 }
