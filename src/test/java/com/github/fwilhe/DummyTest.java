249d260c2b4167297cbc9a2616ac519f0f339fae
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2021-01-28 18:31:21 +0100

    Initial commit

---
+++ src/test/java/com/github/fwilhe/DummyTest.java
@@ -0,0 +1,11 @@
+package com.github.fwilhe;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DummyTest {
+    @Test
+    public void test() {
+        assertNotNull(new Dummy());
+    }
+}
